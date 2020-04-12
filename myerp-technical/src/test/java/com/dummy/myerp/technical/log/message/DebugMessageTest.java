18fd08dbf9f5e5e63b064270f965f12af9282d6e
GregGaoter <greg.gautier@hotmail.com>
2020-04-12 21:42:03 +0200

    Implémentation des tests unitaires de DebugMessage.

---
+++ myerp-technical/src/test/java/com/dummy/myerp/technical/log/message/DebugMessageTest.java
@@ -0,0 +1,111 @@
+package com.dummy.myerp.technical.log.message;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class DebugMessageTest {
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameNullValueNull_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage(null, null);
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("null = null");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameVideValueInteger_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage("", 10);
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo(" = 10");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameLongValueString_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage(
+				"xâkànrCoîVdBScxrËÀaïçÛoYùFmvAÊÎOâÀÔÿQÀÙÜHXDaRWfMSéÙyPÔÊbNÈvCDÂYïîÏnuMüCcépoqçpÊnIYgËeÎzqzXiçôzâëZtUcutFbWàuAidiùÿaPàdVUxvxÛqHgfeôVZHlvGKÊYfZLÂflCRifRôbèBÔsSÉWgChRÜAFLMÂtcJÙQIbÏÀiëWiVYNûàEüÀbéwDneTkrÙUÛêÎÿGàÈjDèëêÔtcYvSnûkÉékCzvZÉXdÛêîÿSgOjlBûâBêHbaéénÎËDPsJTdûOYslÏIFéoËLiquÛÉÊGySpïImuKüÊpbÇFrÎqCHdÊûTÀNERÀQùUÿOwîvkVxPcëoïjehÔSôUîHÜKÏLuëÉuuegcPQÛîÙMÀXzMÙVülÔRââOeôÛZYlDçÂçÊIùSüTKÊMïPËÈàikuJdroçwgèDqgÉvÎuvêèTàfÏGnsQëÛùeÉmBBühnÏzxQpÛÜfNhfUîÎsàreRcqGÇâéËvHÎUvÔXVZÀNfyéÊPrMZÂgaKwhBÇOûTEeÜûzêyylÏéFéHjËüXwüOpWugèËèVôwâÛWNBjKPaÛFIJÂÿNÎyWTGlJlmtDVÉxÿcESçxPTÀôgÔÂYfKaÙngpyÜÜùÇBïzNâpHJùÇXLGUSJjëQVIqLzlÔÜÔjËFkXbutxÈhUAÉqZsïXÙwÂjëkhûÈWÇEHutVçmésaÂîBbgsüwUyZÊÿËmzÜëÈPbgêAMLiDDeIÙPôïPCÏîVsiQÀAIhÔOyZcUÿSübjêîTÉÏÇÀêHEûçtïàèFAjrmStZjGÙGGüQoàÈbÈûehGèOYèïYMÈFnmüRkyRQJpsEÈÔLWJIÏùYôowcBëRxCXôàAGoÏÿmiEMôNûDXFaçiÇômÇîGplEJUrxEÙÇdOrKÎWKèBwÿàIÂÉfyÈmÊNëÇkLsÎYHCyTaÉTÿÉnrFîcOèâïFÂKDÿéCJëISèpâQAKêAÎRdMzûZËNHêBhünçÇOzùdôEîjÊêqtÀïhïÏËZsËxÏÈToLeLayrÈorbiWIWùÛùèRÛRqwLùwokTQqkqnVhJmlCKJQmÜpKeâDfëAÜAâÙjsOfvÂÙXMxWÎowPdtpEMÇàdÜûÂUÔzùCdêÜaAtLkaNçtlvhNcEXç",
+				"Message");
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo(
+				"xâkànrCoîVdBScxrËÀaïçÛoYùFmvAÊÎOâÀÔÿQÀÙÜHXDaRWfMSéÙyPÔÊbNÈvCDÂYïîÏnuMüCcépoqçpÊnIYgËeÎzqzXiçôzâëZtUcutFbWàuAidiùÿaPàdVUxvxÛqHgfeôVZHlvGKÊYfZLÂflCRifRôbèBÔsSÉWgChRÜAFLMÂtcJÙQIbÏÀiëWiVYNûàEüÀbéwDneTkrÙUÛêÎÿGàÈjDèëêÔtcYvSnûkÉékCzvZÉXdÛêîÿSgOjlBûâBêHbaéénÎËDPsJTdûOYslÏIFéoËLiquÛÉÊGySpïImuKüÊpbÇFrÎqCHdÊûTÀNERÀQùUÿOwîvkVxPcëoïjehÔSôUîHÜKÏLuëÉuuegcPQÛîÙMÀXzMÙVülÔRââOeôÛZYlDçÂçÊIùSüTKÊMïPËÈàikuJdroçwgèDqgÉvÎuvêèTàfÏGnsQëÛùeÉmBBühnÏzxQpÛÜfNhfUîÎsàreRcqGÇâéËvHÎUvÔXVZÀNfyéÊPrMZÂgaKwhBÇOûTEeÜûzêyylÏéFéHjËüXwüOpWugèËèVôwâÛWNBjKPaÛFIJÂÿNÎyWTGlJlmtDVÉxÿcESçxPTÀôgÔÂYfKaÙngpyÜÜùÇBïzNâpHJùÇXLGUSJjëQVIqLzlÔÜÔjËFkXbutxÈhUAÉqZsïXÙwÂjëkhûÈWÇEHutVçmésaÂîBbgsüwUyZÊÿËmzÜëÈPbgêAMLiDDeIÙPôïPCÏîVsiQÀAIhÔOyZcUÿSübjêîTÉÏÇÀêHEûçtïàèFAjrmStZjGÙGGüQoàÈbÈûehGèOYèïYMÈFnmüRkyRQJpsEÈÔLWJIÏùYôowcBëRxCXôàAGoÏÿmiEMôNûDXFaçiÇômÇîGplEJUrxEÙÇdOrKÎWKèBwÿàIÂÉfyÈmÊNëÇkLsÎYHCyTaÉTÿÉnrFîcOèâïFÂKDÿéCJëISèpâQAKêAÎRdMzûZËNHêBhünçÇOzùdôEîjÊêqtÀïhïÏËZsËxÏÈToLeLayrÈorbiWIWùÛùèRÛRqwLùwokTQqkqnVhJmlCKJQmÜpKeâDfëAÜAâÙjsOfvÂÙXMxWÎowPdtpEMÇàdÜûÂUÔzùCdêÜaAtLkaNçtlvhNcEXç = Message");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameTabValueNull_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage("\t", null);
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("\t = null");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameRetourLigneValueList_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage("\n", Arrays.asList(-10.5, 3.26));
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("\n = [-10.5, 3.26]");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageName1EspaceValueNull_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage(" ", null);
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("  = null");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageName2EspacesValueNormale_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage("  ", BigDecimal.valueOf(20050L, 2));
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("   = 200.50");
+	}
+
+	@Test
+	public void getFormattedMessage_DefaultMessageNameNormalValueNull_returnsStrinfOfDefaultMessage() {
+		// GIVEN
+		DebugMessage debugMessage = new DebugMessage("Nom", null);
+
+		// WHEN
+		String actualString = debugMessage.getFormattedMessage();
+
+		// THEN
+		assertThat(actualString).isEqualTo("Nom = null");
+	}
+
+}
