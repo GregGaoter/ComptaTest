2ec8700af0cbcb95b33f7232962010ad88b6998e
GregGaoter <greg.gautier@hotmail.com>
2020-06-07 19:29:41 +0200

    Implémentation des tests unitaires de SequenceEcritureComptableRM.SequenceEcritureComptableRM() et mapRow(ResultSet, int).

--- myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRM.java
+++ myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRM.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.RowMapper;
 import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import com.dummy.myerp.technical.log.message.DebugMessage;
 import com.dummy.myerp.technical.log.message.EntreeMessage;
+import com.dummy.myerp.technical.log.message.SortieMessage;
 
 /**
  * {@link RowMapper} de {@link SequenceEcritureComptable}
@@ -19,19 +20,36 @@ public class SequenceEcritureComptableRM implements RowMapper<SequenceEcritureCo
 	/** Logger Log4j pour la classe */
 	private static final Logger LOGGER = LogManager.getLogger(SequenceEcritureComptableRM.class);
 
+	/**
+	 * {@link SequenceEcritureComptable}
+	 */
+	private SequenceEcritureComptable sequenceEcritureComptable;
+
+	/**
+	 * Constructeur. Instancie le {@link SequenceEcritureComptable}.
+	 */
+	public SequenceEcritureComptableRM() {
+		sequenceEcritureComptable = new SequenceEcritureComptable();
+	}
+
 	@Override
 	public SequenceEcritureComptable mapRow(ResultSet pRS, int pRowNum) throws SQLException {
 		LOGGER.trace(new EntreeMessage());
-		LOGGER.debug(new DebugMessage("ResultSet pRS", pRS));
-		LOGGER.debug(new DebugMessage("int pRowNum", pRowNum));
-		SequenceEcritureComptable vBean = new SequenceEcritureComptable();
-		LOGGER.debug(new DebugMessage("(ResultSet) pRS.getString(\"journal_code\")", pRS.getString("journal_code")));
-		vBean.setJournalCode(pRS.getString("journal_code"));
-		LOGGER.debug(new DebugMessage("(ResultSet) pRS.getInt(\"annee\")", pRS.getInt("annee")));
-		vBean.setAnnee(pRS.getInt("annee"));
-		LOGGER.debug(new DebugMessage("(ResultSet) pRS.getInt(\"derniere_valeur\")", pRS.getInt("derniere_valeur")));
-		vBean.setDerniereValeur(pRS.getInt("derniere_valeur"));
-		return vBean;
+		if (pRS == null) {
+			sequenceEcritureComptable = null;
+		} else {
+			LOGGER.debug(new DebugMessage("int pRowNum", pRowNum));
+			LOGGER.debug(
+					new DebugMessage("(ResultSet) pRS.getString(\"journal_code\")", pRS.getString("journal_code")));
+			sequenceEcritureComptable.setJournalCode(pRS.getString("journal_code"));
+			LOGGER.debug(new DebugMessage("(ResultSet) pRS.getInt(\"annee\")", pRS.getInt("annee")));
+			sequenceEcritureComptable.setAnnee(pRS.getInt("annee"));
+			LOGGER.debug(
+					new DebugMessage("(ResultSet) pRS.getInt(\"derniere_valeur\")", pRS.getInt("derniere_valeur")));
+			sequenceEcritureComptable.setDerniereValeur(pRS.getInt("derniere_valeur"));
+		}
+		LOGGER.trace(new SortieMessage());
+		return sequenceEcritureComptable;
 	}
 
 }
---
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRMTest.java
@@ -0,0 +1,80 @@
+package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
+
+@ExtendWith(MockitoExtension.class)
+public class SequenceEcritureComptableRMTest {
+
+	private SequenceEcritureComptableRM sequenceEcritureComptableRM;
+
+	@BeforeEach
+	private void init() {
+		sequenceEcritureComptableRM = new SequenceEcritureComptableRM();
+	}
+
+	@AfterEach
+	private void undef() {
+		sequenceEcritureComptableRM = null;
+	}
+
+	// === SequenceEcritureComptableRM() ===
+
+	@Test
+	public void SequenceEcritureComptableRM_createsSequenceEcritureComptable() {
+		SequenceEcritureComptable sequenceEcritureComptable = (SequenceEcritureComptable) ReflectionTestUtils
+				.getField(sequenceEcritureComptableRM, "sequenceEcritureComptable");
+		assertThat(sequenceEcritureComptable).isNotNull();
+	}
+
+	// === mapRow(ResultSet, int) ===
+
+	private static Stream<Arguments> getArgumentsPourMapRowReturnsSequenceEcritureComptable() {
+		ResultSet resultSet = Mockito.mock(ResultSet.class);
+		return Stream.of(Arguments.of(resultSet, Integer.MIN_VALUE), Arguments.of(null, Integer.MAX_VALUE),
+				Arguments.of(null, 0), Arguments.of(resultSet, -356327592), Arguments.of(resultSet, 669260762));
+	}
+
+	@ParameterizedTest
+	@MethodSource("getArgumentsPourMapRowReturnsSequenceEcritureComptable")
+	public void mapRow_returnsCompteComptable(ResultSet resultSet, int rowNum) throws SQLException {
+		// GIVEN
+		if (resultSet != null) {
+			when(resultSet.getInt(anyString())).thenReturn(1);
+			when(resultSet.getString(anyString())).thenReturn("string");
+		}
+
+		// WHEN
+		sequenceEcritureComptableRM.mapRow(resultSet, rowNum);
+		SequenceEcritureComptable sequenceEcritureComptable = (SequenceEcritureComptable) ReflectionTestUtils
+				.getField(sequenceEcritureComptableRM, "sequenceEcritureComptable");
+
+		// THEN
+		if (resultSet != null) {
+			assertThat(sequenceEcritureComptable.getJournalCode()).isEqualTo("string");
+			assertThat(sequenceEcritureComptable.getAnnee()).isEqualTo(1);
+			assertThat(sequenceEcritureComptable.getDerniereValeur()).isEqualTo(1);
+		} else {
+			assertThat(sequenceEcritureComptable).isNull();
+		}
+	}
+
+}
